688d2f6d66348465c33872029085028c73ea6a05
kelin-xycs <kelin_xycs@163.com>
2022-01-16 03:54:42 +0800

    Signed-off-by: kelin-xycs <kelin_xycs@163.com>

---
+++ PointsRotate/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PointsRotate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtPointsCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtThreadCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(715, 304);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(527, 33);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(131, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "测试";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // txtPointsCount
+            // 
+            this.txtPointsCount.Location = new System.Drawing.Point(117, 35);
+            this.txtPointsCount.Name = "txtPointsCount";
+            this.txtPointsCount.Size = new System.Drawing.Size(117, 21);
+            this.txtPointsCount.TabIndex = 7;
+            this.txtPointsCount.Text = "100";
+            this.txtPointsCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "点 的 数量";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "万";
+            // 
+            // txtThreadCount
+            // 
+            this.txtThreadCount.Location = new System.Drawing.Point(358, 35);
+            this.txtThreadCount.Name = "txtThreadCount";
+            this.txtThreadCount.Size = new System.Drawing.Size(128, 21);
+            this.txtThreadCount.TabIndex = 10;
+            this.txtThreadCount.Text = "4";
+            this.txtThreadCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "线程数";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(679, 33);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(695, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "注 ：  线程编号按执行完成的先后顺序排列，比如 第 1 个执行完的线程叫 “线程 1”，第 2 个执行完的线程叫 “线程 2” 。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtThreadCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPointsCount);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Points Rotate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtPointsCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtThreadCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PointsRotate/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace PointsRotate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const float _sin = 0.5F;
+        private const float _cos = 1.732F / 2;
+
+        private Point[] points = null;
+        private List<Result> resultList = null;
+
+        private int pointsCount;
+        private int threadCount;
+
+        private DateTime totalBeginTime;
+        private DateTime totalEndTime;
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.pointsCount = (int)(float.Parse(txtPointsCount.Text) * 10000);
+
+                if (points == null || points.Length != this.pointsCount)
+                {
+                    CreatePoints(this.pointsCount);
+                }
+
+                
+
+                Rotate();
+
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Rotate()
+        {
+            this.threadCount = int.Parse(txtThreadCount.Text);
+
+            int length = this.pointsCount / threadCount;
+
+            int beginIndex = 0;
+
+            resultList = new List<Result>();
+            this.totalBeginTime = DateTime.Now;
+
+            for (int i = 0; i<threadCount; i++)
+            {
+                Para para = new Para();
+                para.beginIndex = beginIndex;
+                if (i == threadCount - 1)
+                    para.endIndex = pointsCount - 1;
+                else
+                    para.endIndex = beginIndex + length - 1;
+
+                beginIndex = para.endIndex + 1;
+
+                Thread thread = new Thread(new ParameterizedThreadStart(Rotate));
+                thread.Start(para);
+            }
+            
+        }
+
+        private void Rotate(object o)
+        {
+            Para para = (Para)o;
+
+            DateTime beginTime = DateTime.Now;
+
+            for (int i = para.beginIndex; i <= para.endIndex; i++)
+            {
+                Point p = points[i];
+
+                // 围绕 z 轴 旋转
+                p.x = p.x * _cos - p.y * _sin;
+                p.y = p.y * _cos + p.x * _sin;
+
+                // 围绕 y 轴 旋转
+                p.x = p.x * _cos - p.z * _sin;
+                p.z = p.z * _cos + p.x * _sin;
+
+                // 围绕 x 轴 旋转
+                p.y = p.y * _cos - p.z * _sin;
+                p.z = p.z * _cos + p.y * _sin;
+
+            }
+
+            DateTime endTime = DateTime.Now;
+
+            double t = (endTime - beginTime).TotalMilliseconds;
+
+            lock (this.resultList)
+            {
+                this.resultList.Add(new Result(para.endIndex - para.beginIndex + 1, t));
+
+                if (this.resultList.Count == this.threadCount)
+                {
+                    this.totalEndTime = endTime;
+                    ShowResult();
+                }
+            }
+        }
+
+        private void ShowResult()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int sumPointsCount = 0;
+
+            for (int i = 0; i < this.resultList.Count; i++)
+            {
+                Result r = this.resultList[i];
+                sb.Append("线程 " + ( i + 1 ) + " 执行三维旋转 " + r.pointsCount + " 个点， 耗时 ： " + r.t + " 毫秒\r\n");
+
+                sumPointsCount += r.pointsCount;
+
+                //if (maxT < r.t)
+                //    maxT = r.t;
+            }
+
+            double totalT = (this.totalEndTime - this.totalBeginTime).TotalMilliseconds;
+
+            sb.Append("合计 ： " + this.resultList.Count + " 线程 执行三维旋转 共 " + sumPointsCount + " 个点， 耗时 ： " + totalT + " 毫秒， "
+                + "每秒可执行 " + 1000D / totalT + " 次\r\n\r\n");
+
+           
+
+            WriteMsg(sb.ToString());
+
+            //WriteMsg("HashTable 开始时间：" + beginTime.ToString("HH:mm:ss.fff") + " 结束时间：" + endTime.ToString("HH:mm:ss.fff")
+            //    + " 耗时：" + t + " 毫秒，执行 三维旋转 " + pointsCount + " 个点，每秒可执行 " + 1000 / t + " 次 。");
+        }
+
+        private void CreatePoints(int pointsCount)
+        {
+            lock (this)
+            {
+                points = new Point[pointsCount];
+
+                for (int i=1; i<points.Length; i++)
+                {
+                    points[i].x = points[i - 1].x + 1;
+                    points[i].y = points[i - 1].y + 1;
+                    points[i].z = points[i - 1].z + 1;
+                }
+            }
+        }
+
+        private void WriteMsg(string msg)
+        {
+            this.BeginInvoke(new Action(() => { textBox1.AppendText(msg + "\r\n\r\n"); }));
+        }
+
+        private class Para
+        {
+            public int beginIndex;
+            public int endIndex;
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+        }
+    }
+
+    internal struct Point
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    internal struct Result
+    {
+        public int pointsCount;
+        public double t;
+
+        public Result(int pointsCount, double t)
+        {
+            this.pointsCount = pointsCount;
+            this.t = t;
+        }
+    }
+}
